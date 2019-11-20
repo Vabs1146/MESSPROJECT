cb51892caf64f02cc713d323a051f1fd6672470d
Vabs1146 <48979405+Vabs1146@users.noreply.github.com>
2019-11-20 18:48:50 +0530

    initialize

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  username:string;
+  password:string;
+
+  constructor(private router:Router) {
+    
+   }
+
+  ngOnInit() {
+    
+  }
+
+  validate(){
+      alert("sucess");
+  }
+}
